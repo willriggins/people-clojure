441ad34ce18b056f313d32048cdb03b4aeeb7272
willriggins <williamriggins2013@gmail.com>
2016-07-06 11:46:35 -0400

    convert to web app

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,8 +1,11 @@
 (ns people-clojure.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn -main []
+(defn read-people []
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line] (str/split line #","))
@@ -10,7 +13,34 @@
         header (first people)
         people (rest people)
         people (map (fn [line] (zipmap header line))
-                 people)
-        people (filter (fn [line] (= "Brazil" (get line "country")))
                  people)]               
     people))
+
+(defn people-html [people]
+  [:html
+   [:body
+    [:ol
+     (map (fn [person]
+            [:li (str (get person "first_name") " " (get person "last_name"))])
+        people)]]])
+
+(defn filter-by-country [people country]
+  (filter (fn [person]
+            (= country (get person "country")))
+    people))
+     
+(c/defroutes app
+  (c/GET "/:country{.*}" [country]
+    (let [people (read-people)
+          people (if (= 0 (count country))
+                   people
+                   (filter-by-country people country))]
+      (h/html (people-html people)))))
+
+(defonce server (atom nil))
+
+(defn -main []
+  (if @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
+  
